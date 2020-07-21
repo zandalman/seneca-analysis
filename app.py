69d9baa014b3466f75faa740e5796a7bf2eeea54
Zachary Andalman <zandalman@gmail.com>
2020-07-21 09:24:14 -0500

    Not using database (overkill). Instead using cache. Issue with trying to serialize subprocess object.

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ import os
 import time
 import numpy as np
 from werkzeug import secure_filename
-import jsonpickle
+from werkzeug.contrib.cache import SimpleCache
 from plots import *
-
 
 # initialize and configure Flask
 app = Flask(__name__)
@@ -20,15 +19,13 @@ app.config.update(
     UPLOAD_FOLDER=os.path.join(app.root_path, "uploads")
 )
 flask_sijax.Sijax(app)
+cache = SimpleCache()
 
 
 # set global variables
 PLOT_DATA_PATH = os.path.join(app.root_path, "plot_data")
 ROUTINES_PATH = os.path.join(app.root_path, "routines")
 
-def report_status(obj_response, container_id, msg):
-    """Send a message to an HTML element."""
-    obj_response.html_append("#%s" % container_id, "%s<br/>" % html.escape(msg))
 
 # do one analysis iteration
 def analysis_step(obj_response):
@@ -60,7 +57,7 @@ def analysis_step(obj_response):
 
 
 def add_routine(obj_response, files, form_values):
-    routines = jsonpickle.decode(session["routines"])
+    routines = cache.get("routines")
     if "routine" not in files:
         report_status(obj_response, "status", "Upload unsuccessful.")
         return
@@ -75,39 +72,12 @@ def add_routine(obj_response, files, form_values):
     elif filename in [routine.name for routine in routines]:
         report_status(obj_response, "status", "A routine with the name '%s' already exists." % filename)
     else:
-        routines.append(Routine(filename))
+        routines.append(Routine(app.config["UPLOAD_FOLDER"], filename))
         file_data.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
         obj_response.html_append("#routine-list", "<li class='routine' id='%s'>%s</li>" % (routines[-1].id, filename))
         report_status(obj_response, "status", "Upload of '%s' successful." % filename)
-    session["routines"] = jsonpickle.encode(routines)
+    cache.set("routines", routines)
 
-
-class Routine(object):
-
-    def __init__(self, filename):
-        self.name = filename
-        self.path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
-        self.id = gen_id("f", filename)
-        self.running = False
-
-    def stop(self, obj_response, user_init=False):
-        self.running = False
-        if user_init:
-            self.process.terminate()
-            obj_response.call("adjust_routine_class", [self.id, False])
-            report_status(obj_response, "status", "'%s' terminated successfully." % self.name)
-        else:
-            stdout, stderr = self.process.communicate()
-            if "Error" in stdout.decode("utf-8"):
-                obj_response.call("adjust_routine_class", [self.id, True])
-                report_status(obj_response, "status", "'%s' error: '%s'." % (self.name, stdout.decode("utf-8")))
-            else:
-                obj_response.call("adjust_routine_class", [self.id, False])
-                report_status(obj_response, "status", "'%s' completed successfully." % self.name)
-
-    def start(self):
-        self.process = subprocess.Popen(["python", self.path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        self.running = True
 
 class SijaxHandlers(object):
 
@@ -115,7 +85,7 @@ class SijaxHandlers(object):
     def stop_analysis(obj_response):
         """Stop the analysis."""
         print(session)
-        session["analysis_on"] = False
+        cache.set("analysis_on", False)
         report_status(obj_response, "status", "Analysis stopped")
         obj_response.call("reset_timer")
 
@@ -123,13 +93,13 @@ class SijaxHandlers(object):
     def pause_analysis(obj_response):
         """Pause the analysis."""
         print(session)
-        session["analysis_on"] = False
+        cache.set("analysis_on", False)
         report_status(obj_response, "status", "Analysis paused")
         obj_response.call("stop_timer")
 
     @staticmethod
     def remove_routine(obj_response, file_ids):
-        routines = jsonpickle.decode(session)
+        routines = cache.get("routines")
         filenames = []
         for file_id in file_ids:
             routine = [routine for routine in routines if routine.id == file_id][0]
@@ -144,33 +114,33 @@ class SijaxHandlers(object):
             report_status(obj_response, "status", "%s routines removed." % len(file_ids))
         else:
             report_status(obj_response, "status", "%s removed." % ", ".join(filenames))
-        session["routines"] = jsonpickle.encode(routines)
+        cache.set("routines", routines)
 
     @staticmethod
     def stop_routine(obj_response, file_ids):
-        routines = jsonpickle.decode(session["routines"])
+        routines = cache.get("routines")
         for file_id in file_ids:
             routine = [routine for routine in routines if routine.id == file_id][0]
             if routine.running:
                 routine.stop(obj_response, user_init=True)
-        session["routines"] = jsonpickle.encode(routines)
+        cache.set("routines", routines)
 
     @staticmethod
     def run_routine(obj_response, file_id):
-        routines = jsonpickle.decode(session["routines"])
+        routines = cache.get("routines")
         routine = [routine for routine in routines if routine.id == file_id][0]
         routine.start()
         routine.process.wait()
         if routine.running:
             routine.stop(obj_response)
-        session["routines"] = jsonpickle.encode(routines)
+        cache.set("routines", routines)
 
 class SijaxCometHandlers(object):
 
     @staticmethod
     def analyse(obj_response, paused, period):
         """Start the analysis."""
-        session["analysis_on"] = True
+        cache.set("analysis_on", True)
         print(session)
         if paused:
             report_status(obj_response, "status", "Analysis restarted")
@@ -183,7 +153,7 @@ class SijaxCometHandlers(object):
         obj_response.call("start_timer") # start the timer
         yield obj_response
         give_warning = True
-        while session["analysis_on"]:
+        while cache.get("analysis_on"):
             step_start_time = time.time()
             yield from analysis_step(obj_response)
             step_time = time.time() - step_start_time
@@ -199,13 +169,11 @@ class SijaxCometHandlers(object):
 def main():
     """Generate the main page."""
     filenames = os.listdir(app.config["UPLOAD_FOLDER"])
-    routines = [Routine(filename) for filename in filenames]
+    routines = [Routine(app.config["UPLOAD_FOLDER"], filename) for filename in filenames]
     routines_dict = {routine.id: routine.name for routine in routines}
-    # initialize session
-    session.update(
-        analysis_on=False,
-        routines=jsonpickle.encode(routines)
-    )
+    # initialize cache
+    cache.set("routines", routines)
+    cache.set("analysis_on", False)
     # Register Sijax upload handlers
     form_init_js = ''
     form_init_js += g.sijax.register_upload_callback('add-routine-form', add_routine)
--- plots.py
+++ plots.py
@@ -1,12 +1,21 @@
 
-import json, html, uuid, subprocess
+import json, uuid, subprocess, os
+from html import escape
+
 
 def gen_id(marker, seed):
     return marker + uuid.uuid5(uuid.NAMESPACE_DNS, seed).hex
 
+
 def remove_plots(obj_response):
     """Remove all plots and data tables."""
     obj_response.html("#plots-container, #plot-list", "")
+
+
+def report_status(obj_response, container_id, msg):
+    """Send a message to an HTML element."""
+    obj_response.html_append("#%s" % container_id, "%s<br/>" % escape(msg))
+
 
 class Data(object):
 
@@ -34,7 +43,7 @@ class Plot(Data):
     def create(self, obj_response):
         """Create an item in the plot list and initialize the plot."""
         obj_response.html_append("#plots-container", "<div id='%s' class='plot-container' title='%s' style='display: none;'></div>" % (self.id, self.description))
-        obj_response.html_append("#%s" % self.routine_list_id, "<li class='plot-list-item invisible' data-id='%s'>%s - %s</li>" % (self.id, html.escape(self.name), self.type))
+        obj_response.html_append("#%s" % self.routine_list_id, "<li class='plot-list-item invisible' data-id='%s'>%s - %s</li>" % (self.id, escape(self.name), self.type))
         yield obj_response
         obj_response.call("init_img", [self.url, self.id])
         yield obj_response
@@ -49,13 +58,13 @@ class Table(Data):
     def __init__(self, plot_data):
         super().__init__(plot_data)
         self.data = json.loads(plot_data["data"])
-        self.caption = "<caption>%s (%s)</caption>" % (html.escape(self.name), self.file)
+        self.caption = "<caption>%s (%s)</caption>" % (escape(self.name), self.file)
 
     def generate_table_HTML(self):
         """Generate HTML for the table."""
         table_body_HTML = ""
         for param in sorted(self.data.keys()):
-            table_body_HTML += "<tr><th>%s</th><td>%s</td></tr>" % (html.escape(str(param)), html.escape(str(self.data[param])))
+            table_body_HTML += "<tr><th>%s</th><td>%s</td></tr>" % (escape(str(param)), escape(str(self.data[param])))
         table_HTML = "<table>%s<tbody>%s</tbody></table>" % (self.caption, table_body_HTML)
         return table_HTML
 
@@ -68,10 +77,39 @@ class Table(Data):
         """Create an item in the plot list and initialize the table"""
         table_container_HTML = "<div class='table-container' id='%s' title='%s' style='display: none;'>%s</div>" % (self.id, self.description, self.generate_table_HTML())
         obj_response.html_append("#plots-container", table_container_HTML)
-        obj_response.html_append("#%s" % self.routine_list_id, "<li class='plot-list-item invisible' data-id='%s'>%s - table</li>" % (self.id, html.escape(self.name)))
+        obj_response.html_append("#%s" % self.routine_list_id, "<li class='plot-list-item invisible' data-id='%s'>%s - table</li>" % (self.id, escape(self.name)))
         yield obj_response
         obj_response.call("init_table", [self.id])
         yield obj_response
+
+
+class Routine(object):
+
+    def __init__(self, folder, filename):
+        self.name = filename
+        self.path = os.path.join(folder, filename)
+        self.id = gen_id("f", filename)
+        self.running = False
+        self.process = None
+
+    def stop(self, obj_response, user_init=False):
+        self.running = False
+        if user_init:
+            self.process.terminate()
+            obj_response.call("adjust_routine_class", [self.id, False])
+            report_status(obj_response, "status", "'%s' terminated successfully." % self.name)
+        else:
+            stdout, stderr = self.process.communicate()
+            if "Error" in stdout.decode("utf-8"):
+                obj_response.call("adjust_routine_class", [self.id, True])
+                report_status(obj_response, "status", "'%s' error: '%s'." % (self.name, stdout.decode("utf-8")))
+            else:
+                obj_response.call("adjust_routine_class", [self.id, False])
+                report_status(obj_response, "status", "'%s' completed successfully." % self.name)
+
+    def start(self):
+        self.process = subprocess.Popen(["python", self.path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        self.running = True
 
 
 obj_types = {"plot": Plot, "table": Table, "image": Plot}
